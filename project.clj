21e82d5bf4f60f068abf9b8bc58463e12a2b1cd3
Mike Blume <michael.blume@climate.com>
2015-06-14 13:00:01 -0700

    build tests

--- project.clj
+++ project.clj
@@ -7,5 +7,5 @@
         :url "https://github.com/jkk/honeysql"}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0-RC1"]
                                   [org.clojure/clojurescript "0.0-3308"]]
-                   :cljsbuild {:builds [{:source-paths ["src"]}]}
+                   :cljsbuild {:builds [{:source-paths ["src" "test"]}]}
                    :plugins [[lein-cljsbuild "1.0.6"]]}})
